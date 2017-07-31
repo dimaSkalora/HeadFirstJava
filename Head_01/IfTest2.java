4c98ea9fa985bdd15894d543faef036e69e94477
dimaSkalora <timon2@ukr.net>
2017-07-31 21:49:33 +0300

    Head_01

---
+++ Head_01/IfTest2.java
@@ -0,0 +1,11 @@
+
+public class IfTest2 {
+    public static void main(String[] args) {
+        int x=2;
+        if(x==3)
+            System.out.println("ч должен равнятся 3");
+        else
+            System.out.println("ч не равен 3");
+        System.out.println("Эта строка выполняется в любом случиии");
+    }
+}
---
+++ Head_01/cycles/While_1.java
@@ -0,0 +1,11 @@
+package cycles;
+
+public class While_1 {
+    public static void main(String[] args) {
+        int x=4;
+        while(x>3){
+            System.out.println(x);
+            x=x-1;
+        }
+    }
+}
