f528d52b60b8e30ffb173ddaf0e8ce42955989be
dimaSkalora <timon2@ukr.net>
2017-08-02 20:44:24 +0300

    Head_05

---
+++ Head_05/head_05/sea_battle/SimpleDotCom.java
@@ -0,0 +1,28 @@
+package head_05.sea_battle;
+
+public class SimpleDotCom {
+    int[] locationCells;
+    int numOfHits;
+
+    String checkYourself(String stringGuess){
+        int guess = Integer.parseInt(stringGuess);
+        String result = "Мимо";
+
+        for(int cell: locationCells){
+            if(guess == cell){
+                result ="Попал";
+                numOfHits++;
+                break;
+            }
+        }
+
+        if(numOfHits == locationCells.length)
+            result = "Потопил";
+        System.out.println(result);
+        return result;
+    }
+
+    public void setLocationCells(int[] locationCells) {
+        this.locationCells = locationCells;
+    }
+}
